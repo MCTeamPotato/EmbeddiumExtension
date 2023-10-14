74b0d13b31b38d2652bda4eabe379b1145513dcf
Kasualix <670067575@qq.com>
2023-10-14 19:25:55 +0800

    let the mod sleep on server side

--- src/main/java/me/flashyreese/mods/embeddiumextension/client/SodiumExtraClientMod.java
+++ src/main/java/me/flashyreese/mods/embeddiumextension/client/SodiumExtraClientMod.java
@@ -3,8 +3,12 @@ package me.flashyreese.mods.embeddiumextension.client;
 import me.flashyreese.mods.embeddiumextension.client.gui.HudRenderImpl;
 import me.flashyreese.mods.embeddiumextension.client.gui.SodiumExtraGameOptions;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -35,6 +39,7 @@ public class SodiumExtraClientMod {
     }
 
     public SodiumExtraClientMod() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         MinecraftForge.EVENT_BUS.register(new HudRenderImpl());
     }
 }
